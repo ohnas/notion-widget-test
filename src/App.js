11a144cadd917904e59c3198d9c454565de8397f
ohnas <ohnas12@gmail.com>
2024-04-05 16:20:34 +0900

    clock test3

--- src/App.js
+++ src/App.js
@@ -7,14 +7,22 @@ function App() {
   const [time, setTime] = useState(new Date());
   
   useEffect(() => {
-    // 1분마다 시간을 업데이트하는 타이머를 설정합니다.
+    // 현재 시간을 '시:분' 형태로 초기 설정합니다.
+    const currentTime = new Date();
+    currentTime.setSeconds(0); // 초를 0으로 설정하여 분 단위로만 시간을 표시합니다.
+    setTime(currentTime);
+
+    // 1분(60000밀리초)마다 시간을 업데이트하는 타이머를 설정합니다.
     const timerId = setInterval(() => {
-      setTime(new Date());
+      const newTime = new Date(time.getTime() + 60000);
+      setTime(newTime);
     }, 60000);
 
     // 컴포넌트가 언마운트될 때 타이머를 정리합니다.
-    return () => clearInterval(timerId);
-  }, []);
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [time]);
 
   const hours = time.getHours()
   const minutes = time.getMinutes();
