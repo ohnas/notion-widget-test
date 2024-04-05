0d1f73162441017f5ad794f87eb1d2c47d671efe
ohnas <ohnas12@gmail.com>
2024-04-05 16:37:00 +0900

    clear comment

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import styles from './App.module.css';
 
 
 function App() {
-  // 현재 시간을 state로 관리합니다.
   const [time, setTime] = useState(new Date());
   
   useEffect(() => {
-    // 현재 시간을 '시:분' 형태로 초기 설정합니다.
     const currentTime = new Date();
-    currentTime.setSeconds(0); // 초를 0으로 설정하여 분 단위로만 시간을 표시합니다.
+    currentTime.setSeconds(0);
     setTime(currentTime);
 
     // 1분(60000밀리초)마다 시간을 업데이트하는 타이머를 설정합니다.
