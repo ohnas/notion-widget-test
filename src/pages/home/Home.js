9848ccdfadb154f0e02ee458646a3e6e9acbeb0b
ohnas <ohnas12@gmail.com>
2024-04-17 12:50:31 +0900

    img upload

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,15 @@
 import { Link } from 'react-router-dom';
 import styles from './Home.module.css'
+import test from '../../assets/images/test.png'
 
 function Home() {
     return(
         <div className={styles.container}>
             <div className={styles.grid_continer}>
                 <div className={styles.item}>
-                    <Link to={"/flipclock"}>
+                    <Link to={"/notion-widget-test/flipclock"}>
                         <div className={styles.item_img}>
-                            <span>위젯 이미지</span>
+                            <img src={test} alt='test'></img>
                         </div>
                         <div className={styles.item_title}>
                             <span className={styles.title}>Flip Clock</span>
