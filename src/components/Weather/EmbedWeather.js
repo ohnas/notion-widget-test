bbd50e79230b6197f4372f14c15c9a6aab173470
ohnas <ohnas12@gmail.com>
2024-05-30 14:47:25 +0900

    EmbedWeather

--- src/components/Weather/EmbedWeather.js
+++ src/components/Weather/EmbedWeather.js
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
 import styles from './EmbedWeather.module.css';
 
-const API_KEY = process.env.REACT_APP_CURRENT_WEATHER_API_KEY;
+const API_KEY = process.env.REACT_APP_WEATHER_API_KEY;
 
 function EmbedWeather() {
     const [weather, setWeather] = useState({});
+    const [region, setRegion] = useState({});
     const [searchParams] = useSearchParams();
 
     const backgroundColor = searchParams.get('background');
@@ -14,19 +15,15 @@ function EmbedWeather() {
     const lon = searchParams.get('lon');
     
     function getWeather(lat, lon) {
-        fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`)
+        fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}&exclude=minutely,hourly,alerts,daily&units=metric`)
         .then((response) => response.json())
         .then((data) => {
-            const temp = data.main.temp;
-            const name = data.name;
-            const country = data.sys.country;
-            const weatherIcon = data.weather[0].icon;
+            const temp = data.current.temp;
+            const weatherIcon = data.current.weather[0].icon;
             const weatherIconUrl = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
-            const weatherMain = data.weather[0].main;
+            const weatherMain = data.current.weather[0].main;
             setWeather({
                 temp : temp,
-                name : name,
-                country : country,
                 weatherIcon : weatherIcon,
                 weatherIconUrl : weatherIconUrl,
                 weatherMain : weatherMain,
@@ -36,24 +33,36 @@ function EmbedWeather() {
             alert('Something went wrong', error);
         })
     }
+
+    function getGeo(lat, lon) {
+        fetch(`https://api.openweathermap.org/geo/1.0/reverse?lat=${lat}&lon=${lon}&appid=${API_KEY}&limit=1`)
+        .then((response) => response.json())
+        .then((data) =>{
+            setRegion(data[0]);
+        })
+        .catch((error) => {
+            alert('Something went wrong', error);
+        })
+    }
     
     useEffect(() => {
         if(lat !== "" && lat !== null && lon !== "" && lon !== null) {
+            getGeo(lat, lon);
             getWeather(lat, lon);
         }
     },[])
 
     return (
         <div className={styles.container}>
-            {Object.keys(weather).length === 0 ? 
+            {Object.keys(weather).length === 0 && Object.keys(region).length === 0 ? 
                 <div className={styles.load}>
                     <span>Loading...</span>
                 </div>
                 :
                 <div className={styles.item} style={backgroundColor === "" || backgroundColor === null ? null : {'borderColor':backgroundColor}}>
                     <div className={styles.region} style={textColor === "" || textColor === null ? null : {'color':textColor}}>
-                        <span>{weather.name}</span>
-                        <span>{weather.country}</span>
+                        <span>{region.name}</span>
+                        <span>{region.country}</span>
                     </div>
                     <div className={styles.weather}>
                         <img src={weather.weatherIconUrl} alt='weatherIcon'></img>
